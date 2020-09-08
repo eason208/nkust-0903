285ff4abe57efcc2b09eda76468100dc31623b2b
EasonXie <eason0110116@gmail.com>
2020-09-08 10:07:18 +0800

    2222222

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+ <style>
+    body{
+        font-family: 微軟正黑體;
+    }
+ </style>
+
+  <title>謝呈易的0903</title>
+</head>
+<body style="background-color:powderblue;">
+     <h2>謝呈易的0903練習網站</h2>
+    <hr>
+    <?php include "menu.php";?> 
+    <hr>
+<?php include "footer.php";?>
+
+</body>
+</html>
